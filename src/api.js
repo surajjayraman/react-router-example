6270f216384c1cb31ab31e3fa55a5a60f831c006
surajjayraman <suraj.jayraman@gmail.com>
2021-11-30 15:51:55 -0500

    Create mock users database.

--- src/api.js
+++ src/api.js
@@ -3,12 +3,23 @@ import { v4 as uuidv4 } from 'uuid';
 const idOne = uuidv4();
 const idTwo = uuidv4();
 
+// pseudo database
 let users = {
     [idOne]: {
         id: idOne,
         firstName: 'Suraj',
         lastName: 'Nair',
         isDeveloper: true
-    } 
-
+    },
+    [idTwo]: {
+        id: idTwo,
+        firstName: 'John',
+        lastNmae: 'Doe',
+        isDeveloper: false
+    }
 }
+
+
+
+
+
