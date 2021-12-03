0c3d9dbe5f3fc61074b826fa5be2e802771d849f
surajjayraman <suraj.jayraman@gmail.com>
2021-12-02 21:28:32 -0500

    Update the UserData component to fetch the mock data from api.

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
 import { getUsers, createUser, updateUser, deleteUser } from '../api';
 
 const UserData = () => {
-    console.log(getUsers());
+    const [users, setUsers] = useState([]);    
+
+    // fetch the mock data from api
+    useEffect(() => {
+        getUsers()
+        .then(result => { 
+            console.log(result);
+            setUsers(result);
+        })
+        .catch(error => console.log(error));
+
+    }, []);
+  
+
+    const getDeveloperText = (isDeveloper) => `is ${isDeveloper ? 'a' : 'not a'} developer`;
     return (
         <div>
-            <h2>Hello from User Data Mock API!!</h2>
+            <ul>
+                {users.map((user) => {
+                const developerText = getDeveloperText(user.isDeveloper);
+
+                return (
+                    <li key={user.id}>
+                    {user.firstName} {user.lastName} {developerText}
+                    </li>
+                );
+                })}
+            </ul>    
         </div>
     );
 }
