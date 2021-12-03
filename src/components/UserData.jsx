03e3b1a192ed5781507ed0a46c936b38d1a54b3e
surajjayraman <suraj.jayraman@gmail.com>
2021-12-03 13:40:56 -0500

    Add form to capture user input data.

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 import { getUsers, createUser, updateUser, deleteUser } from '../api';
 
 const UserData = () => {
-    const [users, setUsers] = useState([]);    
+    const [users, setUsers] = useState([]);   
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState(''); 
 
     // fetch the mock data from api
     useEffect(() => {
@@ -13,7 +15,25 @@ const UserData = () => {
         })
         .catch(error => console.log(error));
 
-    }, []);
+    }, []);    
+
+    const handleChangeFirstName = (event) => {
+        setFirstName(event.target.value);
+    };
+
+    const handleChangeLastName = (event) => {
+        setLastName(event.target.value);
+    };
+
+    // Add a new user to the mock database
+    const handleCreate = async (event) => {
+        
+        event.preventDefault();
+    
+        createUser({ firstName, lastName, isDeveloper: false })
+        .then(result => console.log(result))
+        .catch (error => console.log(error));
+    };
   
 
     const getDeveloperText = (isDeveloper) => `is ${isDeveloper ? 'a' : 'not a'} developer`;
@@ -29,7 +49,26 @@ const UserData = () => {
                     </li>
                 );
                 })}
-            </ul>    
+            </ul>  
+            <hr />
+
+            <span>Create User:</span>
+            <p></p>
+
+            <form onSubmit={handleCreate}>
+                <label>
+                First Name:
+                <input type="input" onChange={handleChangeFirstName} />
+                </label>
+
+                <label>
+                Last Name:
+                <input type="input" onChange={handleChangeLastName} />
+                </label>
+
+                <button type="submit">Create</button>
+            </form>  
+
         </div>
     );
 }
